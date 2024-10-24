124c1a52bf70d3fbf93cc03fa0938c66f6cb35da
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-25 02:16:42 +0600

    added friend and friend files and modified app file of jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Counter from './Counter';
 import Team from './Team';
 import Users from './Users';
+import Friends from './Friends';
 
 
 function App() {
@@ -21,6 +22,8 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+
+      <Friends></Friends>
 
       <Users></Users>
 
