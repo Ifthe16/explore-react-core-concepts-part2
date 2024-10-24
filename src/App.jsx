1f5af2fdbe42f57da762c6287e8b73be63a05a22
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-24 23:04:52 +0600

    added users file of jsx and fetched data using effect and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Counter from './Counter';
 import Team from './Team';
+import Users from './Users';
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+
+      <Users></Users>
 
       <Team></Team>
 
